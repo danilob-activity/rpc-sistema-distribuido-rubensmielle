170e3f2686a9ab7f14d6d8a269b9eb52736c05db
rubensmielle <35923288+rubensmielle@users.noreply.github.com>
2019-10-04 23:33:21 -0300

    Add files via upload

--- RPCClient.java
+++ RPCClient.java
@@ -6,22 +6,23 @@ class RPCClient
     {
         try
         {
-            InetAddress ia = InetAddress.getLocalHost();
+            InetAddress ia = InetAddress.getByName("0.0.0.0");
             DatagramSocket ds = new DatagramSocket();
             DatagramSocket ds1 = new DatagramSocket(1300);
             System.out.println("\nRPC Client\n");
-            System.out.println("Insira a operação (ex: add, sub, mult, div) e os números que deseja calcular \n");
+            System.out.println("Insira a operação (ex: add, sub, mult, div, mod, pow) e os números que deseja calcular \n");
             while (true)
             {
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 String str = br.readLine();
                 byte b[] = str.getBytes();
                 DatagramPacket dp =  new DatagramPacket(b,b.length,ia,1200);
+                byte b1[] = new byte[500];
                 ds.send(dp);
-                dp = new DatagramPacket(b,b.length);
+                dp = new DatagramPacket(b1,b1.length);
                 ds1.receive(dp);
                 String s = new String(dp.getData(),0,dp.getLength());
-                System.out.println("\nResultado =" + s +"\n");
+                System.out.println("\nResultado = " + s +"\n");
 
             }
 
--- RPCServer.java
+++ RPCServer.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.net.*;
+import java.lang.*;
 class RPCServer
 {
     DatagramSocket ds;
@@ -8,12 +9,14 @@ class RPCServer
     float val1,val2;
     RPCServer()
     {
+        
         try
         {
             ds=new DatagramSocket(1200);
             byte b[]=new byte[4096];
             while(true)
             {
+            result = "(Mielle) ";
             dp=new DatagramPacket(b,b.length);
             ds.receive(dp);
             str=new String(dp.getData(),0,dp.getLength());
@@ -34,26 +37,34 @@ class RPCServer
                 }
             }
             System.out.println(str);
-            InetAddress ia = InetAddress.getLocalHost();
+            //InetAddress ia = InetAddress.getLocalHost();
             if(methodName.equalsIgnoreCase("add"))
             {
-                result= "" + add(val1,val2);
+                result += "" + add(val1,val2);
             }
             else if(methodName.equalsIgnoreCase("sub"))
             {
-                result= "" + sub(val1,val2);
+                result += "" + sub(val1,val2);
             }
             else if(methodName.equalsIgnoreCase("mul"))
             {
-                result= "" + mul(val1,val2);
+                result += "" + mul(val1,val2);
             }
             else if(methodName.equalsIgnoreCase("div"))
             {
-                result= "" + div(val1,val2);
+                result += "" + div(val1,val2);
+            }
+            else if(methodName.equalsIgnoreCase("mod"))
+            {
+                result += "" + mod(val1,val2);
+            }
+            else if(methodName.equalsIgnoreCase("pow"))
+            {
+                result += "" + pow(val1,val2);
             }
             byte b1[]=result.getBytes();
             DatagramSocket ds1 = new DatagramSocket();
-            DatagramPacket dp1 = new DatagramPacket(b1,b1.length,InetAddress.getLocalHost(),1300);
+            DatagramPacket dp1 = new DatagramPacket(b1,b1.length,dp.getAddress(),1300);
             System.out.println("result : "+result+"\n");
             ds1.send(dp1);
             }
@@ -78,6 +89,14 @@ class RPCServer
     public float div(float val3, float val4)
     {
         return val3/val4;
+    }
+    public float mod(float val3, float val4)
+    {
+        return val3%val4;
+    }
+    public double pow(double val3, double val4)
+    {
+        return Math.pow(val3,val4);
     }
     public static void main(String[] args)
     {
